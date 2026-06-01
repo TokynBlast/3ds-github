6ea338976daf84067d96d62c6f1f0b4e2f66cf5e
RuriMei <tokynblast@gmail.com>
2026-06-01 14:54:14 -0700

    Remove build comment

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 fn main() {
   let devkitpro = std::env::var("DEVKITPRO").unwrap_or_else(|_| "/opt/devkitpro".to_string());
-
-  // Pass the search path for libctru binary to cargo
   println!("cargo:rustc-link-search=native={}/libctru/lib", devkitpro);
   println!("cargo:rustc-link-lib=static=ctru");
 }
