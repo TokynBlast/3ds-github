4eaa19d720ea25d1ea1d804580c0784ac6fe2436
RuriMei <tokynblast@gmail.com>
2026-06-02 13:46:34 -0700

    Add background rendering

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,26 @@ fn main() {
     while apt.main_loop() {
         let top_buff = top_screen.raw_framebuffer();
         let bottom_buff = bottom_screen.raw_framebuffer();
+        // 3DS works with BGR, not RGB (this is stupid ;-;)
         unsafe {
+            // #0F61A5 Background on top screen
+            let mut i = 0;
+            while i < top_bytes {
+                *top_buff.ptr.add(i)     = 0xA5; // Blue
+                *top_buff.ptr.add(i + 1) = 0x61; // Green
+                *top_buff.ptr.add(i + 2) = 0x0F; // Red
+                i += 3;
+            }
+
+            // #0F61A5 Background on bottom screen
+            let mut j = 0;
+            while j < bottom_bytes {
+                *bottom_buff.ptr.add(j)     = 0xA5; // Blue
+                *bottom_buff.ptr.add(j + 1) = 0x61; // Green
+                *bottom_buff.ptr.add(j + 2) = 0x0F; // Red
+                j += 3;
+            }
+        }
             std::ptr::copy_nonoverlapping(
               image_bytes.as_ptr(),
                 buffer.ptr,
