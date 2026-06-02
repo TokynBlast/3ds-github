2bc5684bdb8229a1ca76f4b5e17cca951ff1eef1
RuriMei <tokynblast@gmail.com>
2026-06-02 13:39:05 -0700

    Indent build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-  let devkitpro = std::env::var("DEVKITPRO").unwrap_or_else(|_| "/opt/devkitpro".to_string());
-  println!("cargo:rustc-link-search=native={}/libctru/lib", devkitpro);
-  println!("cargo:rustc-link-lib=static=ctru");
+    let devkitpro = std::env::var("DEVKITPRO").unwrap_or_else(|_| "/opt/devkitpro".to_string());
+    println!("cargo:rustc-link-search=native={}/libctru/lib", devkitpro);
+    println!("cargo:rustc-link-lib=static=ctru");
 }
