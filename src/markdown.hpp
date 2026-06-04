a08b18f107f5d05d0f199d0c78f6912d3a9cebde
RuriMei <tokynblast@gmail.com>
2026-06-03 17:49:49 -0700

    Add markdown

---
+++ src/markdown.hpp
@@ -0,0 +1,12 @@
+// This is unimplemented. It will help in rendering markdown.
+
+// // Italic, bold, underline, etc.
+// enum mod {
+//     italic,
+//     underline,
+//     bold,
+// };
+// // This holds what the text is :)
+// // Whether it's a table, bold, etc.!
+// // And it holds the actual text too.
+// struct object {}
