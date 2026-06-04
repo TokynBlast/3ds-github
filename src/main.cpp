8e6ef459104b3f308ae7ce6f7b55acd62721d57d
RuriMei <tokynblast@gmail.com>
2026-06-03 17:39:19 -0700

    Add signed out drawing

--- src/main.cpp
+++ src/main.cpp
@@ -124,7 +124,10 @@ int main() {
         C2D_DrawText(&title, C2D_WithColor, 10.0f, 10.0f, 0.4f, 2.0f, 2.0f, WHITE);
 
         if (!hasInternet()) {
-            C2D_DrawText(&online, C2D_WithColor, 10.0f, 55.0f, 0.5f, 0.7f, 0.7f, WHITE);
+        } else {
+            if (state.acc_stat == CurrentState::AccountStatus::SignedOut) {
+                C2D_DrawRectSolid(100.0f, 20.0f, 0.0f, 90.0f, 30.0f, DARK_BLUE);
+            }
         }
 
         // Fill in bottom screen, then start drawing everything that belongs there
